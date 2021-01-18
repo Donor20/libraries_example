113b35a5ab22acf91e1109ecb6ac6883f2fd0b1f
Donor20 <thisisstory@mail.ru>
2021-01-18 21:53:53 +0300

    .

--- src/pages/LibList.vue
+++ src/pages/LibList.vue
@@ -1,97 +1,47 @@
 <template>
-  <div class="page libList q-pa-sm">
+  <div class="page libList q-pa-sm q-gutter-sm">
     <h5 class="title">Список библиотек</h5>
     <div class="filters">
       <q-input v-model="textFilter"
                class="bordered" placeholder="Начните вбивать название..."
-               borderless clearable
+               borderless clearable debounce="500"
       />
     </div>
-    <div v-if="false" class="flatsPageFilters row">
-      <div class="rangeFilter">
-        <div class="rangeFilterName">КОМНАТЫ</div>
-        <div class="roomsFilter row">
-          <q-btn v-for="ro in roomsOptions" :key="ro.value"
-                 @click="roomsFilter === ro.value ? roomsFilter = null : roomsFilter = ro.value"
-                 class="no-shadow" :class="{ 'activeBtn': roomsFilter === ro.value }"
-                 unelevated no-caps
-          >{{ro.label}}</q-btn>
-        </div>
-      </div>
-      <range-filter v-for="rangeFilter in rangeFilters" :key="rangeFilter.key"
-                    :rangeFilter="rangeFilter"
-      />
-      <div class="btnsFilters">
-        <q-btn @click="getFlats" class="activeBtn" unelevated>
-          Применить
-        </q-btn>
-        <div class="refreshFiltersBtn" @click="resetFilters">
-          <span>сбросить фильтр</span>
-        </div>
-      </div>
-    </div>
-<!--    <q-scroll-area class="scrollArea">-->
-      <q-table
-        class="bordered m-table my-sticky-header-table"
-        :data="data"
-        :columns="columns"
-        separator="cell"
-        flat hide-bottom
-        :rows-per-page-options="[0]"
-      >
-        <template v-slot:header="props">
-        <q-tr :props="props">
-          <q-th v-for="col in props.cols" :key="col.name" :props="props">
-            <span class="text-bold">{{col.label}}</span>
-          </q-th>
+    <q-table
+      class="bordered m-table my-sticky-header-table"
+      :data="libs"
+      :columns="columns"
+      separator="cell"
+      flat hide-bottom
+      :rows-per-page-options="[0]"
+    >
+      <template v-slot:header="props">
+      <q-tr :props="props">
+        <q-th v-for="col in props.cols" :key="col.name" :props="props">
+          <span class="text-bold">{{col.label}}</span>
+        </q-th>
+      </q-tr>
+    </template>
+      <template v-slot:body="props">
+        <q-tr class="cursor-pointer" @click="openPage(props.row)">
+          <q-td v-for="col in props.cols" :key="col.name" :props="props">
+            <div>{{getFormattedRow(props.row)[col.field]}}</div>
+          </q-td>
         </q-tr>
       </template>
-        <template v-slot:body="props">
-          <q-tr class="cursor-pointer" @click="openPage(props.row)">
-            <q-td v-for="col in props.cols" :key="col.name" :props="props">
-              <div>{{getFormattedRow(props.row)[col.field]}}</div>
-            </q-td>
-          </q-tr>
-        </template>
-      </q-table>
-<!--    </q-scroll-area>-->
-    <q-list v-if="false" class="full-height" bordered>
-      <q-scroll-area class="scrollArea">
-        <template v-for="lib of data">
-          <lib-elem :lib="lib" :key="lib._id"></lib-elem>
-        </template>
-      </q-scroll-area>
-    </q-list>
-    <q-scroll-area v-if="false" class="scrollArea">
-      <div class="row flatCards">
-        <flat-card v-for="flat in flats" :key="flat.building_id + '_' + flat.id"
-                   :flat="flat"
-        />
-      </div>
-    </q-scroll-area>
+    </q-table>
   </div>
 </template>
 
 <script>
 import api from '../services/api'
-import FlatCard from '../components/flat-card'
-import RangeFilter from '../components/range-filter'
-import LibElem from '../components/lib-elem'
 
 export default {
   name: 'LibList',
-  components: { LibElem, RangeFilter, FlatCard },
+  components: {},
   data () {
     return {
-      flats: [],
-      roomsFilter: null,
-      roomsOptions: [
-        { label: 'S', value: '?' },
-        { label: '1к', value: 1 },
-        { label: '2к', value: 2 },
-        { label: '3к', value: 3 }
-      ],
-      data: [],
+      libs: [],
       columns: [
         {
           name: 'name',
@@ -111,9 +61,9 @@ export default {
       textFilter: ''
     }
   },
-  computed: {
-    rangeFilters () {
-      return this.$store.getters['filters/ranges']
+  watch: {
+    textFilter () {
+      this.getLibs()
     }
   },
   methods: {
@@ -126,29 +76,14 @@ export default {
         localeName: row.data.general.locale.name
       }
     },
-    resetFilters () {
-      this.roomsFilter = null
-      this.$store.dispatch('filters/resetRanges')
-      this.getFlats()
-    },
-    getFlats () {
-      const filters = { rooms: this.roomsFilter }
-      for (const rangeFilter of this.rangeFilters) {
-        filters[rangeFilter.key] = rangeFilter.value
-      }
-      return api.getFlats(filters).then(flats => {
-        this.flats = flats
-      })
-    },
-    getLibraries () {
-      return api.getLibraries().then(data => {
-        this.data = data.slice(0, 20)
+    getLibs () {
+      return api.getLibs().then(libs => {
+        this.libs = libs.slice(0, 20)
       })
     }
   },
   mounted () {
-    this.getFlats()
-    this.getLibraries()
+    this.getLibs()
   }
 }
 </script>
@@ -174,9 +109,10 @@ $m-border-radius: 5px;
     border-radius: $m-border-radius;
   }
   .m-table {
-    max-height: calc(100% - 122px);
-    /*height: 200px;*/
-    font-size: 16px;
+    max-height: calc(100% - 136px);
+    td, th {
+      font-size: 14px;
+    }
     thead {
       tr th {
         position: sticky;
@@ -197,26 +133,3 @@ $m-border-radius: 5px;
   }
 }
 </style>
-
-<!--<style lang="sass">-->
-<!--  .my-sticky-header-table-->
-<!--    /* height or max-height is important */-->
-<!--    /*height: 310px  !important*/-->
-
-<!--    .q-table__top,-->
-<!--    .q-table__bottom,-->
-<!--    thead tr:first-child th-->
-<!--      /* bg color is important for th; just specify one */-->
-<!--      background-color: #c1f4cd-->
-
-<!--    thead tr th-->
-<!--      position: sticky-->
-<!--      z-index: 1-->
-<!--    thead tr:first-child th-->
-<!--      top: 0-->
-
-<!--    /* this is when the loading indicator appears */-->
-<!--    &.q-table&#45;&#45;loading thead tr:last-child th-->
-<!--      /* height of all previous header rows */-->
-<!--      top: 48px-->
-<!--</style>-->
